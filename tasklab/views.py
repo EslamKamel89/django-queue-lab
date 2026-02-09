c07e76bc17ad8e9546db36b60560c6e52958c7a3
EslamKamel89 <elsamkamelforex@gmail.com>
2026-02-09 13:20:05 +0200

    feat(tasklab): add async task trigger UI and POST handler with simulated delay

--- tasklab/views.py
+++ tasklab/views.py
@@ -1,8 +1,18 @@
+from time import sleep
+
 from django.http import HttpRequest, HttpResponse
+from django.http.response import JsonResponse
 from django.shortcuts import render
+from django.utils.decorators import method_decorator
 from django.views import View
+from django.views.decorators.csrf import csrf_exempt
 
 
+@method_decorator(csrf_exempt, "dispatch")
 class HomeView(View):
     def get(self, request: HttpRequest):
         return render(request, "tasklab/home.html")
+
+    def post(self, request: HttpRequest):
+        sleep(5)
+        return JsonResponse({"slow_task": "finished"})
