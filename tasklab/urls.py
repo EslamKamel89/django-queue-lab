6e00ed44a916700e2c8ff27d9c851bd185fb9374
EslamKamel89 <elsamkamelforex@gmail.com>
2026-02-12 22:21:26 +0200

    feat(tasklab): integrate Celery async task triggering with result polling endpoint and UI updates

--- tasklab/urls.py
+++ tasklab/urls.py
@@ -5,4 +5,5 @@ from .views import *
 urlpatterns: list[URLPattern] = [
     path("", HomeView.as_view(), name="home"),
     path("health", HealthView.as_view(), name="health"),
+    path("slow-task/<str:task_id>", SlowTaskResultView.as_view()),
 ]
--- tasklab/views.py
+++ tasklab/views.py
@@ -1,3 +1,4 @@
+import json
 from time import sleep
 
 from django.http import HttpRequest, HttpResponse
@@ -6,6 +7,9 @@ from django.shortcuts import render
 from django.utils.decorators import method_decorator
 from django.views import View
 from django.views.decorators.csrf import csrf_exempt
+from django_celery_results.models import TaskResult
+
+from tasklab.tasks import slow_task
 
 
 @method_decorator(csrf_exempt, "dispatch")
@@ -14,10 +18,19 @@ class HomeView(View):
         return render(request, "tasklab/home.html")
 
     def post(self, request: HttpRequest):
-        sleep(5)
-        return JsonResponse({"slow_task": "finished"})
+        task_id = slow_task.delay("hello")  # type: ignore
+        print(task_id.__str__())
+        return JsonResponse(
+            {"message": "Slow task triggered", "task_id": task_id.__str__()}
+        )
 
 
 class HealthView(View):
     def get(self, request: HttpRequest):
         return JsonResponse({"status": "ok"})
+
+
+class SlowTaskResultView(View):
+    def get(self, request: HttpRequest, task_id: str):
+        task = TaskResult.objects.get(task_id=task_id)
+        return JsonResponse({"data": json.loads(task.result or "{}")})
